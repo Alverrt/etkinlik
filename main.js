a9ec186cd363b9957b999e9ab14fab7b5341f689
Batuhan Birkan <batuhanbirkan6@gmail.com>
2022-07-04 22:55:31 +0300

    konsola yazi yazdirdim

---
+++ main.js
@@ -0,0 +1,11 @@
+const kutu = document.getElementById("tekKutu")
+
+kutu.addEventListener("mouseover", () => {
+    kutu.style.backgroundColor = "purple"
+})
+
+kutu.addEventListener("mouseleave", () => {
+    kutu.style.backgroundColor = "red"
+})
+
+console.log("aasdfasdfas")
